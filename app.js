536e9aeb8c9b630595acbc64137093daa33cb9b0
pawelmat142 <pawelmat142@gmail.com>
2022-07-17 15:32:32 +0200

    add https and letsencrypt

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const express = require('express')
+const fs = require('fs')
+const http = require('http')
+const https = require('https')
+const path = require('path')
 const { createProxyMiddleware } = require('http-proxy-middleware')
 
+
+const app = express();
+const port = 80
+const httpsPort = 443
+
+
+// PROXY CONFIG
+
 const options = {
-    target: 'http://drawit.click',
+    target: 'https://drawit.click',
     changeOrigin: true,
     router: {
         'jackpot.drawit.click': 'http://localhost:8001',
@@ -11,11 +23,25 @@ const options = {
     }
 }
 
-const app = express();
-const port = 80
-
 app.use('/', createProxyMiddleware(options))
 
-app.listen(port, () => {
-    console.log('listening on: ' + port)
+
+// HTTPS CONFIG
+
+const httpsServer = https.createServer({
+    key: fs.readFileSync("../../../etc/letsencrypt/archive/drawit.click/privkey1.pem"),
+    cert: fs.readFileSync("../../../etc/letsencrypt/archive/drawit.click/fullchain1.pem"),
+    ca: fs.readFileSync("../../../etc/letsencrypt/archive/drawit.click/chain1.pem")
+}, app)
+
+httpsServer.listen(httpsPort, () => {
+    console.log('HTTPS listening on port: ' + httpsPort) 
 })
+
+
+// redirect HTTP server
+
+const httpApp = express()
+httpApp.all('*', (req, res) => res.redirect("https://" + req.headers.host + req.url ))
+const httpServer = http.createServer(httpApp)
+httpServer.listen(port, () => console.log('HTTP listening on port: ' + port))
